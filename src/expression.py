92b4106abfa4621034ac9d9110d87cbd928612db
Ethan Tan <2b03.2012085.EthanTan@gmail.com>
2022-01-18 11:50:24 +0800

    added Expression class

---
+++ src/expression.py
@@ -0,0 +1,22 @@
+from typing import Union
+
+
+class Expression:
+    def __init__(self, expr: str, result: Union[float, int]) -> None:
+        self.__expr = expr
+        self.__result = result
+
+    def __lt__(self, otherExpression: 'Expression') -> bool:
+        return self.__result < otherExpression.get_result()
+
+    def __le__(self, otherExpression: 'Expression') -> bool:
+        return self.__result <= otherExpression.get_result()
+
+    def __gt__(self, otherExpression: 'Expression') -> bool:
+        return self.__result > otherExpression.get_result()
+
+    def __ge__(self, otherExpression: 'Expression') -> bool:
+        return self.__result >= otherExpression.get_result()
+
+    def get_result(self) -> Union[float, int]:
+        return self.__result
