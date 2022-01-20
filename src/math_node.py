5c6a8f7238d9e44e58467adbd876d01bbec8e618
Ethan Tan <2b03.2012085.EthanTan@gmail.com>
2022-01-20 22:33:49 +0800

    fixed bugs; refactored code

--- src/math_node.py
+++ src/math_node.py
@@ -1,19 +1,27 @@
 from typing import Any
 from .node import Node
+from abc import abstractmethod
 
 
 class MathNode(Node):
     def __init__(self, value, symbol, func) -> None:
-        super().__init__(value)
+        super().__init__(value=value, width=len(str(value)))
         self._symbol = symbol
         self._func = func
 
     def __call__(self) -> Any:
-        if self.left is not None:
-            self.left()
-        if self.right is not None:
-            self.right()
-        self._value = self._func(self.left, self.right)
+        if self._left is not None and self._right is not None:
+            self._left()
+            self._right()
+            self._value = self._func(self._left._value, self._right._value)
 
     def __str__(self) -> str:
         return str(self._symbol)
+
+    @abstractmethod
+    def __lt__(self, otherNode) -> bool:
+        pass
+
+    @abstractmethod
+    def __gt__(self, otherNode) -> bool:
+        pass
