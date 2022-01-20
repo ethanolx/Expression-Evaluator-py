1ff7893c1e3314386432ad3194bd957325aa3da9
Ethan Tan <2b03.2012085.EthanTan@gmail.com>
2022-01-20 17:39:35 +0800

    tree

--- src/tree.py
+++ src/tree.py
@@ -1,6 +1,14 @@
-# type: ignore
 from typing import Any, Callable, Optional
+
+from .exceptions import InvalidOptionError
 from .node import Node
+from enum import Enum
+
+
+class TreeTraversalOrder(Enum):
+    PRE_ORDER = 0
+    IN_ORDER = 1
+    POST_ORDER = 2
 
 
 class Tree:
@@ -9,25 +17,41 @@ class Tree:
         self.root: Optional[Node] = None
         self.currentPointer: Optional[Node] = None
         self.__depth_symbol = depth_symbol
+        self.__print_mode = 'h'
+        self.__print_traversal_order: TreeTraversalOrder = TreeTraversalOrder.IN_ORDER
 
-    def dfs_in(self, node: Node, func: Callable, depth: int = 0):
-        if node is not None:
-            yield from self.dfs_in(node.right, func=func, depth=depth + 1)
-            yield func(node, depth)
-            yield from self.dfs_in(node.left, func=func, depth=depth + 1)
-
-    def __str__(self) -> str:
-        representation = []
-        for s in self.dfs_in(node=self.root, func=lambda n, d: self.__depth_symbol * d + str(n)):
-            representation.append(s)
-        return '\n'.join(representation)
-
-    def __call__(self) -> Any:
-        if self.root is not None:
-            self.root()
-            return self.root._value
-        return None
+    def print_inorder(self):
+        def __internal_recursive(node: Optional[Node], depth: int = 0):
+            if node is not None:
+                __internal_recursive(node=node.right, depth=depth + 1)
+                print(self.__depth_symbol * depth + str(node))
+                __internal_recursive(node=node.left, depth=depth + 1)
 
     def reset(self):
         self.root = None
         self.currentPointer = None
+
+    def print_vertical(self) -> str:
+        self.root.update_widths()
+        current_nodes = [self.root]
+        next_nodes = []
+        representation = []
+        while list(filter(lambda n: isinstance(n, Node), current_nodes)):
+            row = []
+            for n in current_nodes:
+                if isinstance(n, Node):
+                    row.append(n.padded_display())
+                    if n.left is None and n.right is None:
+                        next_nodes.append(n.get_width())
+                    else:
+                        next_nodes.append(n.left)
+                        next_nodes.append(n.right)
+                else:
+                    row.append(' ' * n)
+                    next_nodes.append(n)
+
+            current_nodes = next_nodes
+            next_nodes = []
+            representation.append(' '.join(row))
+        return '\n'.join(representation)
+
